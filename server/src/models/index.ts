c718d47e7f7f180e20860f87ce4a69bd8ee7fc63
IOUser755 <dieter.st@gmx.de>
2025-10-27 22:02:05 +0100

    Add agent and agent template models

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,5 +1,14 @@
 export { default as Tenant } from './Tenant.js';
 export { default as User } from './User.js';
+export { default as Agent } from './Agent.js';
+export { default as AgentTemplate } from './AgentTemplate.js';
 
 export type { ITenant } from './Tenant.js';
 export type { IUser } from './User.js';
+export type {
+  IAgent,
+  IWorkflowStep,
+  IAgentConfiguration,
+  IAgentMetrics,
+} from './Agent.js';
+export type { IAgentTemplate } from './AgentTemplate.js';
